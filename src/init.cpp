b4e2e5f2b88ce28faea6759eca88ea667c5af90d
1notchdev <mobileweb1989@hotmail.com>
2021-02-25 02:47:01 -0500

    IBD update

--- src/init.cpp
+++ src/init.cpp
@@ -34,6 +34,7 @@
 #endif
 
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/replace.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <openssl/crypto.h>
